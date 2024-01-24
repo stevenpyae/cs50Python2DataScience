2b2011f6862e3fb604fb80ea3e638d8db8f6bd6a
steven.kyaw <steven.kyaw@gmail.com>
2024-01-24 16:04:00 +0800

    PSet0

--- cs50Python/Wk0FuncVariables/PSet0.py
+++ cs50Python/Wk0FuncVariables/PSet0.py
@@ -14,15 +14,19 @@ print(int(input("m: ")) * 300000000 * 30000000)
 # Tip Calculator <-- Teach you float conversion
 
 def main():
-    dollars = dollars_to_float(input("How much was the meal? "))
+    dollars = dollars_to_float(input("How much was the meal? ").lstrip("$"))
     percent = percent_to_float(input("What percentage would you like to tip? "))
     tip = dollars * percent
-    print(f"Leave ${tip:.2f}")  # <---- 2 decimal palces
+    print(f"Leave ${tip:.2f}")
 
 
 def dollars_to_float(d):
+    # TODO
     return float(d)
 
 
 def percent_to_float(p):
-    return float(p) / 100
+    # TODO
+    return float(p)/100
+
+
