19c3ccc13678b6e5b4385e154e6f238d9671d2e7
steven.kyaw <steven.kyaw@gmail.com>
2024-01-24 16:05:47 +0800

    lstrip rstrip

--- cs50Python/Wk0FuncVariables/PSet0.py
+++ cs50Python/Wk0FuncVariables/PSet0.py
@@ -15,7 +15,7 @@ print(int(input("m: ")) * 300000000 * 30000000)
 
 def main():
     dollars = dollars_to_float(input("How much was the meal? ").lstrip("$"))
-    percent = percent_to_float(input("What percentage would you like to tip? "))
+    percent = percent_to_float(input("What percentage would you like to tip? ").rstrip("%"))
     tip = dollars * percent
     print(f"Leave ${tip:.2f}")
 
